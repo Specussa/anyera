53b16ccfe9c69526d2f9eced5c3fe221ce5e2443
Spec Ussa <serega.ussa@gmail.com>
2023-04-30 18:20:01 +0300

    dev 1.0

--- js/main.js
+++ js/main.js
@@ -86,3 +86,77 @@ document.querySelector('.header__burger').addEventListener('click', function() {
     });
   })
  // end navbar
+
+ // start carusel
+class Carusel {
+  constructor(node) {
+    this.node = node
+    this.list = null
+    this._counter = 0
+    this.sides = 0
+    this.step = 0
+    this.shift = 0
+    this.findParts()
+    this.prepare()
+    this.listen()
+  }
+  
+  findParts() {
+    this.list = this.node.querySelector('[data-slider-list]')
+    this.items = [...this.list.children]
+    this.step_buttons = [...this.node.querySelectorAll('[data-slider-step-button]')].map((button) => {
+        return {
+        node: button,
+        value: !!button.dataset.sliderStepButton ? +button.dataset.sliderStepButton: 1
+      }
+    })
+  }
+  
+  prepare() {
+    this.sides = this.items.length
+    this.step = 2 * Math.PI / this.sides
+    this.shift = Math.PI / 2
+    this.counter = 0
+  }
+  
+  listen() {
+    this.step_buttons.forEach(button => {
+        button.node.addEventListener('click', () => {
+        this.counter = this.counter + button.value
+      })
+    })
+  }
+  
+  listenKeys(e) {
+    if(e.key === 'ArrowLeft') {
+      this.counter = this.counter - 1
+    } else if(e.key === 'ArrowRight') {
+      this.counter = this.counter + 1
+    }
+  }
+  
+  notify() {
+    this.items.forEach((item, i) => {
+      const pos = (i + this.counter) * this.step + this.shift
+      const cos = Math.cos(pos)
+      const sin = Math.sin(pos)
+      const scale = Math.max(0.3, (sin + 1) / 2)
+      item.style.setProperty('--cos', (cos + 0.05))
+      item.style.setProperty('--sin', (sin - 0.9))
+      item.style.setProperty('--scale', scale)
+    })
+  }
+  
+  get counter() {
+    return this._counter
+  }
+  
+  set counter(new_value) {
+    this._counter = new_value < 0 ? this.sides + new_value : new_value % this.sides
+    this.notify()
+  }
+}
+
+const carusel = new Carusel(document.querySelector('.slider'))
+
+ // end carusel
